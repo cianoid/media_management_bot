1d7425df7074a067a87fbbc927a5241a15772167
Igor Shatava <cianoid@yandex.ru>
2022-03-13 19:29:52 +0900

    remove moderator command from admin.py

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -6,14 +6,6 @@ async def cmd_commands_admin(message: types.Message):
     await message.reply('Команды админа')
 
 
-async def cmd_commands_moderator(message: types.Message):
-    await message.reply('Команды модератора')
-
-
-def register_handlers_admin(
-        dp: Dispatcher, admin_ids: list[int], moderator_ids: list[int]):
+def register_handlers_admin(dp: Dispatcher, admin_ids: list[int]):
     dp.register_message_handler(
         cmd_commands_admin, IDFilter(user_id=admin_ids), commands="commands")
-    dp.register_message_handler(
-        cmd_commands_moderator, IDFilter(user_id=moderator_ids),
-        commands="commands")
