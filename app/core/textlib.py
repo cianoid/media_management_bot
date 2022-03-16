f0ce7803f26c8f3f49dfd9b8994bc4330feefab6
Igor Shatava <cianoid@yandex.ru>
2022-03-17 07:21:35 +0900

    add text

--- app/core/textlib.py
+++ app/core/textlib.py
@@ -1,5 +1,5 @@
-LOG_CRITICAL_ENV_CHECK_FAILED = ('Отсутствует обязательная переменная '
-                                 '"{parameter}". Бот остановлен')
+LOG_CRITICAL_ENV_CHECK_FAILED = (
+    'Отсутствует обязательная переменная "{parameter}". Бот остановлен')
 LOG_DEBUG_ENV_CHECK = 'Проверка env-переменных для подключения'
 LOG_INFO_LAUNCH = 'Бот запущен!'
 
@@ -11,21 +11,29 @@ MSG_SUGGEST_START = (
     f'Пришлите <b>{TEXT_HELP_TEXT}</b>, <b>{TEXT_HELP_PHOTO}</b> или '
     f'<b>{TEXT_HELP_DOCUMENT}</b>, которые хотите отправить на рассмотрение '
     f'или напишите <b>отмена</b> (или команду /cancel) для отмены')
-MSG_SUGGEST_CANCEL = ('ОК! Если что-то захотите прислать, просто введите '
-                      'команду /suggest снова')
+MSG_SUGGEST_CANCEL = (
+    'ОК! Если что-то захотите прислать, просто введите команду /suggest снова')
 MSG_SUGGEST_END = 'Спасибо! Взяли {} на рассмотрение'
 
 MSG_SMTH_IS_WRONG = 'Что-то пошло не так. Попробуйте позже'
-MSG_MODERATION_HAS_ALREADY_TAKEN_PLACE = ('Пользователь {} уже отмодерировал '
-                                          'этот материал {}')
-MSG_APPROVE_MODERATOR = ('Вы одобрили предложение! Мы сообщим пользователю о '
-                         'результатах модерации')
+MSG_MODERATION_HAS_ALREADY_TAKEN_PLACE = (
+    'Пользователь {} уже отмодерировал этот материал {}')
+MSG_APPROVE_MODERATOR = (
+    'Вы одобрили предложение! Мы сообщим пользователю о результатах модерации')
 MSG_APPROVE_USER = 'Модератор одобрил предложенный материал!'
-MSG_REJECT_MODERATOR = ('Вы отклонили предложение! Мы сообщим пользователю о '
-                        'результатах модерации')
+MSG_REJECT_MODERATOR = (
+    'Вы отклонили предложение! Мы сообщим пользователю о результатах '
+    'модерации')
 MSG_REJECT_USER = 'Модератор отклонил предложенный материал!'
 MSG_USER_SUGGEST = 'Пользователь @{} (user_id={}) предложил {}'
 
 BTN_APPROVE = 'Принять'
 BTN_REJECT = 'Отклонить'
 
+MSG_ADMIN_COMMANDS = (
+    'Команды админа:\n'
+    '/moderator_list - список модераторов\n'
+    '/moderator_add - добавить модератора\n'
+    '/moderatod_delete - удалить модератора')
+MSG_NO_MODERATORS = 'К сожалению, у вас нет модераторов'
+MSG_MODERATOR_LIST = 'Ваши модераторы:\n'
