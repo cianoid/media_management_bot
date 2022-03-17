22ebce4d53b31b3dd11cd818bc8b51febb043421
Igor Shatava <cianoid@yandex.ru>
2022-03-17 21:51:02 +0900

    add decorator is_moderator

---
+++ app/core/decorators.py
@@ -0,0 +1,19 @@
+from functools import wraps
+
+from app.core import textlib as _
+
+
+def is_moderator(dbuser):
+    def decorator_factory(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            message = args[0]
+            tg_user_id = message['from']['id']
+
+            if dbuser.get(tg_user_id=tg_user_id).is_moderator:
+                return await func(*args, **kwargs)
+
+            await message.reply(_.MSG_NO_RIGHTS)
+
+        return wrapper
+    return decorator_factory
