45d351a013ed5066ec1788974510b7cf352a5096
cianoid <cianid@yandex.ru>
2022-03-17 15:24:02 +0900

    move common things to common.py

---
+++ app/core/common.py
@@ -0,0 +1,61 @@
+import logging
+import os
+import re
+
+from dotenv import load_dotenv
+
+import app.core.textlib as _
+from app.core.exceptions import EnvironmentParameterError
+
+
+load_dotenv()
+
+
+ADMINS = [
+    int(chat_id)
+    for chat_id
+    in re.split(' +', os.getenv('ADMIN_IDS', default='0'))]
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+logger = logging.getLogger(__name__)
+
+
+def log_init():
+    log_level = os.getenv('LOG_LEVEL', default='INFO')
+    level = getattr(logging, log_level)
+
+    logs_directory = os.path.join(os.getcwd(), 'logs')
+    file_path = os.path.join(logs_directory, '%s.log' % log_level.lower())
+
+    if not os.path.isdir(logs_directory):
+        os.mkdir(logs_directory)
+
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)s [%(levelname)s] %(message)s')
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(file_path, 'a+')
+
+    logger.setLevel(level)
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+    stream_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    logging.basicConfig(level=log_level)
+
+
+def environment_check():
+    try:
+        logger.debug(_.LOG_DEBUG_ENV_CHECK)
+
+        if TELEGRAM_TOKEN is None:
+            raise EnvironmentParameterError('TELEGRAM_TOKEN')
+        if ADMINS is None:
+            raise EnvironmentParameterError('ADMIN_IDS')
+    except EnvironmentParameterError as parameter:
+        logger.critical(
+            _.LOG_CRITICAL_ENV_CHECK_FAILED.format(parameter=parameter))
+        raise SystemExit
+
+
+def is_admin(tg_user_id):
+    return tg_user_id in ADMINS
--- app/mbot.py
+++ app/mbot.py
@@ -1,70 +1,17 @@
 import asyncio
-import logging
-import os
-import re
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from dotenv import load_dotenv
 
-import app.core.textlib as _
-from app.core.exceptions import EnvironmentParameterError
-from handlers.admin import (register_callbacks_admin,
-                            register_handlers_admin)
-from handlers.common import register_handlers_common
-from handlers.suggestion import (register_callbacks_suggestion,
-                                 register_handlers_suggestion)
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-ADMINS = [
-    int(chat_id)
-    for chat_id
-    in re.split(' +', os.getenv('ADMIN_IDS', default='0'))]
-
-logger = logging.getLogger(__name__)
-
-
-def log_init():
-    log_level = os.getenv('LOG_LEVEL', default='INFO')
-    level = getattr(logging, log_level)
-
-    logs_directory = os.path.join(os.getcwd(), 'logs')
-    file_path = os.path.join(logs_directory, '%s.log' % log_level.lower())
-
-    if not os.path.isdir(logs_directory):
-        os.mkdir(logs_directory)
-
-    formatter = logging.Formatter(
-        '%(asctime)s %(name)s [%(levelname)s] %(message)s')
-    stream_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(file_path, 'a+')
-
-    logger.setLevel(level)
-    logger.addHandler(stream_handler)
-    logger.addHandler(file_handler)
-    stream_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
-    logging.basicConfig(level=log_level)
-
-
-def environment_check():
-    try:
-        logger.debug(_.LOG_DEBUG_ENV_CHECK)
-
-        if TELEGRAM_TOKEN is None:
-            raise EnvironmentParameterError('TELEGRAM_TOKEN')
-        if ADMINS is None:
-            raise EnvironmentParameterError('ADMIN_IDS')
-    except EnvironmentParameterError as parameter:
-        logger.critical(
-            _.LOG_CRITICAL_ENV_CHECK_FAILED.format(parameter=parameter))
-        raise SystemExit
+from app.core.common import log_init, environment_check, TELEGRAM_TOKEN, ADMINS
+from app.handlers.admin import (register_callbacks_admin,
+                                register_handlers_admin)
+from app.handlers.common import register_handlers_common
+from app.handlers.suggestion import (register_callbacks_suggestion,
+                                     register_handlers_suggestion)
 
 
 async def main():
-    load_dotenv()
     log_init()
     environment_check()
 
